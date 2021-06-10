cc26b7f78ba398938300d3bfe32e32dc53518646
cordell <695883280@qq.com>
2021-06-11 00:14:39 +0800

    pulsar后台

---
+++ demo_for_pulsar/src/main/java/com/fujitsu/tti/DemoForPulsarApplication.java
@@ -0,0 +1,13 @@
+package com.fujitsu.tti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForPulsarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoForPulsarApplication.class, args);
+    }
+
+}
---
+++ demo_for_pulsar/src/main/java/com/fujitsu/tti/MQServer/Pulsar.java
@@ -0,0 +1,13 @@
+package com.fujitsu.tti.MQServer;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author cordell
+ * @date 2021/6/10
+ */
+@Slf4j
+public class Pulsar {
+
+
+}
